fb38681a2bcc54ada4c56ada9e5e63843c368e3d
Salimkun <m.alimarrasyid@gmail.com>
2018-10-07 09:01:04 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="src/assets/css/index.css">
+</head>
+<body>
+
+<div id="myDIV" class="header">
+  <h2 style="margin:5px">My List Menu</h2>
+  <input type="text" id="myInput" placeholder="Title...">
+  <span onclick="newElement()" class="addBtn">Add</span>
+</div>
+
+<ul id="myUL">
+  <li>Ayam Gepuk Mak Elo</li>
+  <li class="checked">Nasi Pincuk</li>
+  <li>Nasi Kebuli</li>
+  <li>Ayam Rica -Rica</li>
+  <li>Garang Asem</li>
+  <li>Ayam Bakar</li>
+</ul>
+
+<script src="src/assets/js/index.js"></script>
+
+</body>
+</html>
